872befb05b5acb270cd3e375f8badd9b636b3fa3
batusan <54415339+batusan@users.noreply.github.com>
2020-05-20 01:24:02 +0300

    Proje dosyası ekle.

---
+++ KitapEvi.Uygulamasi/Kitaplik.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace KitapEvi.Uygulamasi
+{
+    public class Kitaplik
+    {
+        private static string kitapadi;
+        private static string yazar;
+        private static DateTime basimtarihi;
+        private static string tur;
+
+        public static string Kitapadi { get => kitapadi; set => kitapadi = value; }
+        public static string Yazar { get => yazar; set => yazar = value; }
+        public static DateTime Basimtarihi { get => basimtarihi; set => basimtarihi = value; }
+        public static string Tur { get => tur; set => tur = value; }
+
+        private static int kitapsayi;//Kullanıcının eklemek istediği sayıyı tanımlıcağımız değişken.
+        private static int tarih; // Basım tarihi tanımlanması sırasında ki aksaklıkları engellemek için kullanıldı.
+
+
+        public Kitaplik(string kitapadi, string yazar, DateTime basimtarihi, string tur)
+        {
+            Kitapadi = kitapadi;
+            Yazar = yazar;
+            Basimtarihi = basimtarihi;
+            Tur = tur;
+        }        
+        
+
+        public static void KitabiYazdir(Kitaplik[] kitapdizisi,int i)
+        {
+            FileStream fs = new FileStream("D:/kitapkayitlari.txt", FileMode.Append, FileAccess.Write, FileShare.Write);
+            StreamWriter sw = new StreamWriter(fs);
+            sw.WriteLine(kitapdizisi[i].KitapDizisi());
+            fs.Flush();
+            sw.Close();
+            fs.Close();
+        }
+
+
+        public static void KitaplariGoruntule()
+        {
+            string line;
+            StreamReader file = new StreamReader(@"D:/kitapkayitlari.txt");
+            while ((line = file.ReadLine()) != null)
+            {
+                Console.WriteLine(line);
+            }
+            file.Close();
+        }
+        public static void KitapEkleme()
+        {
+            Console.WriteLine("Kaç kitap eklemek istiyorsunuz ?");
+            kitapsayi = Convert.ToInt16(Console.ReadLine());
+            Kitaplik[] kitapdizisi = new Kitaplik[kitapsayi];
+
+            for (int i = 0; i < kitapsayi; i++)
+            {
+                Console.WriteLine("Kitap Adını Giriniz : \n");
+                Kitapadi = Console.ReadLine();
+                Console.WriteLine("Kitap Yazarını Giriniz : \n");
+                Yazar = Console.ReadLine();
+                Console.WriteLine("Kitap Basım Yılını Giriniz : \n");
+                do
+                {
+                    tarih = int.Parse(Console.ReadLine());
+                    if (tarih > 2020)
+                    {
+                        Console.WriteLine("Basım Yılını 2020'den Küçük Girmelisiniz.\n Tekrar Giriniz : ");
+                    }
+                } while (tarih > 2020);
+                Basimtarihi = new DateTime(tarih, 1, 1);
+                Console.WriteLine("Kitap Tur Giriniz : ");
+                Tur = Console.ReadLine();
+                Kitaplik kitap = new Kitaplik(Kitapadi, Yazar, Basimtarihi, Tur);
+                kitapdizisi[i] = kitap;
+                KitabiYazdir(kitapdizisi,i);
+            }
+        }
+        public string KitapDizisi() => $"Kitap Adı:{Kitapadi} | Yazar:{Yazar} | Basım Tarihi:{Basimtarihi.Year} | Tur:{Tur}";
+    }
+}
---
+++ KitapEvi.Uygulamasi/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+
+namespace KitapEvi.Uygulamasi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string cevap = "";
+                Console.WriteLine("Merhabalar Kitabevi Sistemine Hoşgeldiniz \nUygulamayı " + DateTime.Now + " tarihinde çalıştırdın.");
+                do {
+                Console.WriteLine("\n \n \n \n" + "-----------------------------------------\n" + "1.Kitapevine kitap eklemek \n2.Kitaplığı görüntülemek.");
+                Console.WriteLine("\nİşlem Yapmak İçin Rakam Giriniz :");
+                char ch = Convert.ToChar(Console.ReadLine());
+                    switch (ch)
+                    {
+                        case '1':
+                            Kitaplik.KitapEkleme();
+                            break;
+                        case '2':
+                            Kitaplik.KitaplariGoruntule();
+                            break;
+                        default:
+                            Console.WriteLine("Menüde sadece 2 seçenek bulunmaktadır , 1 veya 2 seçeneğini kullanınız.");
+                            break;
+                    }
+                    Console.WriteLine("---------------------------------------\nBaşka işlem yapmak istiyor musunuz ? (e-h)");
+                    cevap = Console.ReadLine();
+                } while (cevap == "e");
+            }
+            catch (Exception hata)
+            {
+                Console.WriteLine("Lütfen örnekte ki gibi bir kullanım gerçekleştiriniz.'1' - '2'");
+            }
+        }
+    }
+}
